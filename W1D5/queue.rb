e31772c7c4743ff551c65e8d5b7b756ea3b14175
Codededoc <darrenlin1002@gmail.com>
2018-09-27 23:33:51 -0400

    adds Queue class and basic methods

--- W1D5/queue.rb
+++ W1D5/queue.rb
@@ -1,5 +1,39 @@
 # ## Exercise 2 - Queue
 #
-# Now let's write a `Queue` class. We will need the following instance methods: `enqueue(el)`, `dequeue`, and `peek`.
+# Now let's write a `Queue` class.
+# We will need the following instance methods: `enqueue(el)`, `dequeue`, and `peek`.
 #
 # Test your code to ensure it follows the principle of FIFO.
+
+class Queue
+
+  attr_reader :queue
+
+  def initialize (queue = [])
+    @queue = queue
+  end
+
+  def enqueue(el)
+    queue.push(el)
+  end
+
+  def dequeue
+    queue.shift
+  end
+
+  def peek
+    queue.first
+  end
+
+end
+
+test_queue = Queue.new([1, 2, 3])
+
+print test_queue
+puts
+print test_queue.enqueue(4)
+puts
+print test_queue.dequeue
+puts
+print test_queue.peek
+puts
