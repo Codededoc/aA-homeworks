b45ecc76946c1f9e31a6ef1205ac6ec0d1258022
Codededoc <darrenlin1002@gmail.com>
2018-09-27 22:52:44 -0400

    adds DIY ADTs exercises

---
+++ W1D5/queue.rb
@@ -0,0 +1,5 @@
+# ## Exercise 2 - Queue
+#
+# Now let's write a `Queue` class. We will need the following instance methods: `enqueue(el)`, `dequeue`, and `peek`.
+#
+# Test your code to ensure it follows the principle of FIFO.
---
+++ W1D5/stack.rb
@@ -0,0 +1,31 @@
+# # DIY ADTs
+#
+# After you finish the exercises, or if you get stuck on one of the problems, you can view the solutions [here][adt-solutions].
+#
+# [adt-solutions]: https://github.com/appacademy/curriculum/blob/master/ruby/homeworks/abstract_data_types/solution.rb
+#
+# ## Exercise 1 - Stack
+#
+# Let's write a `Stack` class. To do this, use the following framework:
+#
+# ```ruby
+#   class Stack
+#     def initialize
+#       # create ivar to store stack here!
+#     end
+#
+#     def push(el)
+#       # adds an element to the stack
+#     end
+#
+#     def pop
+#       # removes one element from the stack
+#     end
+#
+#     def peek
+#       # returns, but doesn't remove, the top element in the stack
+#     end
+#   end
+# ```
+#
+# To test that your code works, create a new instance of the Stack class, and play around with adding and removing elements. Remember, a stack follows the principle of LIFO!
